b264742d10452d883c37549e6fdace8616417ba1
Enfe-git <mrkzakrzewski@gmail.com>
2020-09-09 21:44:20 +0100

    uaing Math.Sin()

--- Assets/Oscilator.cs
+++ Assets/Oscilator.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Oscilator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
     //todo remove from inspector later
     [Range(0,1)] [SerializeField] float movementFactor; //0 for not moved, 1 for fully moved.
@@ -21,6 +22,12 @@ public class Oscilator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //set movement factor
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
